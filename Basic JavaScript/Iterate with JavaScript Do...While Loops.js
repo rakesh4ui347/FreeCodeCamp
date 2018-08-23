692c84eed741ae064abcbfe375c25b966eff8f29
Rakesh Kumar <39724733+rakesh4ui347@users.noreply.github.com>
2018-08-24 00:01:25 +0530

    FreeCodeCamp
    Basic JavaScript

---
+++ Basic JavaScript/Iterate with JavaScript Do...While Loops.js
@@ -0,0 +1,35 @@
+var ourArray = [];
+var i = 0;
+do {
+  ourArray.push(i);
+  i++;
+} while (i < 5);
+// [0, 1, 2, 3, 4]
+// *********//
+var ourArray = []; 
+var i = 5;
+while (i < 5) {
+  ourArray.push(i);
+  i++;
+}
+//do not execute the code inside the loop
+// [];
+/// ** do while ***//
+var ourArray = []; 
+var i = 5;
+do {
+  ourArray.push(i);
+  i++;
+} while (i < 5);
+/// ourArray is [5]
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do{
+  myArray.push(i);
+  i++;
+}while (i < 5) ;
