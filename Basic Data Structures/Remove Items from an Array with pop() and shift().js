1ceefe214d7ff95cac72d2d2b690e2f4d39e4129
Rakesh Kumar <39724733+rakesh4ui347@users.noreply.github.com>
2018-08-26 15:19:50 +0530

    Remove Items from an Array with pop() and shift()

--- Basic Data Structures/Remove Items from an Array with pop() and shift().js
+++ Basic Data Structures/Remove Items from an Array with pop() and shift().js
@@ -1,7 +1,7 @@
 /***Both push() and unshift() have corresponding methods that are nearly functional opposites: 
  * //pop() and shift(). As you may have guessed by now, instead of adding, 
  * //pop() removes an element from the end of an array, 
- * //while shift() removes an element from the beginning. 
+ * //while shift() removes an element from the beginning.. 
  * //The key difference between pop() and shift() and their cousins push() and unshift(), 
  * //is that neither method takes parameters, and each only allows an array to be modified by a single element at a time. */
  function popShift(arr) {
