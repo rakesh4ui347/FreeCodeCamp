2cc4d6dcd276b19c9a95d8197bcd1c2a1e3821d7
Rakesh Kumar <39724733+rakesh4ui347@users.noreply.github.com>
2018-08-28 21:48:46 +0530

    Modify an Array Stored in an Object

---
+++ Basic Data Structures/Modify an Array Stored in an Object.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ let nestedObject = {
+  id: 28802695164,
+  date: 'December 31, 2016',
+  data: {
+    totalUsers: 99,
+    online: 80,
+    onlineStatus: {
+      active: 67,
+      away: 13
+    }
+  }
+};
+ */
+/*************** */
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+  };
+  
+  // change code below this line
+  userActivity.data.online=45;
+  // change code above this line
+  
+  console.log(userActivity);
