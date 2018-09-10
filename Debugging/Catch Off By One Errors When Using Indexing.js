c0f4e40d5bcc29dbf7367361a2c470799023f114
Rakesh Kumar <39724733+rakesh4ui347@users.noreply.github.com>
2018-09-10 23:45:16 +0530

    Debugging

---
+++ Debugging/Catch Off By One Errors When Using Indexing.js
@@ -0,0 +1,27 @@
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+let len = alphabet.length;
+for (let i = 0; i <= len; i++) {
+  // loops one too many times at the end
+  console.log(alphabet[i]);
+}
+for (let j = 1; j < len; j++) {
+  // loops one too few times and misses the first character at index 0
+  console.log(alphabet[j]);
+}
+for (let k = 0; k < len; k++) {
+  // Goldilocks approves - this is just right
+  console.log(alphabet[k]);
+}
+
+///
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Fix the line below
+    for (let i = 0; i < len; i++) {
+    // Do not alter code below this line
+      console.log(firstFive[i]);
+    }
+  }
+  
+  countToFive();
---
+++ Debugging/Use typeof to Check the Type of a Variable.js
@@ -0,0 +1,14 @@
+//Here are some examples using typeof:
+
+console.log(typeof ""); // outputs "string"
+console.log(typeof 0); // outputs "number"
+console.log(typeof []); // outputs "object"
+console.log(typeof {}); // outputs "object"
+
+////
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Add your code below this line
+console.log(typeof seven);
+console.log(typeof three);
